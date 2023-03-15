9ac93a9a1c33f38124dd3c129016509829d7ecfb
alriansyah <alriansyah1218@gmail.com>
2023-03-15 20:49:01 +0700

    first migrations upload

---
+++ database/migrations/2023_03_15_124355_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('gender', 20);
+            $table->string('nis', 50);
+            $table->string('no_hp', 50);
+            $table->string('alamat', 100);
+            $table->string('foto', 255)->nullable();
+            $table->string('email', 255);
+            $table->string('password', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
