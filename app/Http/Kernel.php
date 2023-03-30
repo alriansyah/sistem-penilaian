c27aa868825244e386d4c4f79c13ebcb8ef01ea0
alriansyah <alriansyah1218@gmail.com>
2023-03-30 12:45:36 +0700

    Create middleware teacher

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\Admin::class,
-        'student' => \App\Http\Middleware\Student::class
+        'student' => \App\Http\Middleware\Student::class,
+        'teacher' => \App\Http\Middleware\Teacher::class
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use App\Http\Controllers\TeacherDashboardController;
 
 Route::get('/', function () {
     return view('dashboard');
-})->middleware(['auth:student,web,teacher', 'admin']);
+})->middleware(['auth:web,student,teacher', 'admin']);
 
 Route::controller(AuthController::class)->group(function () {
     Route::get('/login', 'login')->name('login')->middleware('guest');
@@ -33,82 +33,82 @@ Route::controller(AuthController::class)->group(function () {
 });
 
 Route::controller(StudentController::class)->group(function () {
-    Route::get('/student', 'index')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/student-detail/{id}', 'show')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/student-add', 'create')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::post('/student', 'store')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/student-edit/{id}', 'edit')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::put('/student/{id}', 'update')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::delete('/student-destroy/{id}', 'destroy')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/student-deleted', 'deletedStudent')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/student/{id}/restore', 'restore')->middleware(['auth:student,web,teacher', 'admin']);
+    Route::get('/student', 'index')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/student-detail/{id}', 'show')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/student-add', 'create')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::post('/student', 'store')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/student-edit/{id}', 'edit')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::put('/student/{id}', 'update')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::delete('/student-destroy/{id}', 'destroy')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/student-deleted', 'deletedStudent')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/student/{id}/restore', 'restore')->middleware(['auth:web,student,teacher', 'admin']);
 });
 
 Route::controller(TeacherController::class)->group(function () {
-    Route::get('/teacher', 'index')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/teacher-detail/{id}', 'show')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/teacher-add', 'create')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::post('/teacher', 'store')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/teacher-edit/{id}', 'edit')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::put('/teacher/{id}', 'update')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::delete('/teacher-destroy/{id}', 'destroy')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/teacher-deleted', 'deletedTeacher')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/teacher/{id}/restore', 'restore')->middleware(['auth:student,web,teacher', 'admin']);
+    Route::get('/teacher', 'index')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/teacher-detail/{id}', 'show')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/teacher-add', 'create')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::post('/teacher', 'store')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/teacher-edit/{id}', 'edit')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::put('/teacher/{id}', 'update')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::delete('/teacher-destroy/{id}', 'destroy')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/teacher-deleted', 'deletedTeacher')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/teacher/{id}/restore', 'restore')->middleware(['auth:web,student,teacher', 'admin']);
 });
 
 Route::controller(JurusanController::class)->group(function () {
-    Route::get('/jurusan', 'index')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/jurusan-detail/{id}', 'show')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/jurusan-add', 'create')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::post('/jurusan', 'store')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/jurusan-edit/{id}', 'edit')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::put('/jurusan/{id}', 'update')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::delete('/jurusan-destroy/{id}', 'destroy')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/jurusan-deleted', 'deletedJurusan')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/jurusan/{id}/restore', 'restore')->middleware(['auth:student,web,teacher', 'admin']);
+    Route::get('/jurusan', 'index')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/jurusan-detail/{id}', 'show')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/jurusan-add', 'create')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::post('/jurusan', 'store')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/jurusan-edit/{id}', 'edit')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::put('/jurusan/{id}', 'update')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::delete('/jurusan-destroy/{id}', 'destroy')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/jurusan-deleted', 'deletedJurusan')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/jurusan/{id}/restore', 'restore')->middleware(['auth:web,student,teacher', 'admin']);
 });
 
 Route::controller(ClassController::class)->group(function () {
-    Route::get('/class', 'index')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/class-detail/{id}', 'show')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/class-add', 'create')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::post('/class', 'store')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/class-edit/{id}', 'edit')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::put('/class/{id}', 'update')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::delete('/class-destroy/{id}', 'destroy')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/class-deleted', 'deletedClass')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/class/{id}/restore', 'restore')->middleware(['auth:student,web,teacher', 'admin']);
+    Route::get('/class', 'index')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/class-detail/{id}', 'show')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/class-add', 'create')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::post('/class', 'store')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/class-edit/{id}', 'edit')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::put('/class/{id}', 'update')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::delete('/class-destroy/{id}', 'destroy')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/class-deleted', 'deletedClass')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/class/{id}/restore', 'restore')->middleware(['auth:web,student,teacher', 'admin']);
 });
 
 Route::controller(MataPelajaranController::class)->group(function () {
-    Route::get('/mapel', 'index')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/mapel-detail/{id}', 'show')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/mapel-add', 'create')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::post('/mapel', 'store')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/mapel-edit/{id}', 'edit')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::put('/mapel/{id}', 'update')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::delete('/mapel-destroy/{id}', 'destroy')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/mapel-deleted', 'deletedMapel')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/mapel/{id}/restore', 'restore')->middleware(['auth:student,web,teacher', 'admin']);
+    Route::get('/mapel', 'index')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/mapel-detail/{id}', 'show')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/mapel-add', 'create')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::post('/mapel', 'store')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/mapel-edit/{id}', 'edit')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::put('/mapel/{id}', 'update')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::delete('/mapel-destroy/{id}', 'destroy')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/mapel-deleted', 'deletedMapel')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/mapel/{id}/restore', 'restore')->middleware(['auth:web,student,teacher', 'admin']);
 });
 
 Route::controller(NilaiController::class)->group(function () {
-    Route::get('/nilai', 'index')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/nilai-detail/{id}', 'show')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/nilai-add', 'create')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::post('/nilai', 'store')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::get('/nilai-edit/{id}', 'edit')->middleware(['auth:student,web,teacher', 'admin']);
-    Route::post('/nilai/{id}', 'update')->middleware(['auth:student,web,teacher', 'admin']);
+    Route::get('/nilai', 'index')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/nilai-detail/{id}', 'show')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/nilai-add', 'create')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::post('/nilai', 'store')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::get('/nilai-edit/{id}', 'edit')->middleware(['auth:web,student,teacher', 'admin']);
+    Route::post('/nilai/{id}', 'update')->middleware(['auth:web,student,teacher', 'admin']);
 });
 
 
 // Dashboard Student
 Route::controller(StudentDashboardController::class)->group(function () {
-    Route::get('/student/dashboard', 'index')->middleware(['auth:student', 'student']);
+    Route::get('/student/dashboard', 'index')->middleware(['auth:student,web,teacher', 'student']);
 });
 
 
 // Dashboard Taecher
 Route::controller(TeacherDashboardController::class)->group(function () {
-    Route::get('/teacher/dashboard', 'index')->middleware('auth:teacher');
+    Route::get('/teacher/dashboard', 'index')->middleware(['auth:teacher,web,student', 'teacher']);
 });
